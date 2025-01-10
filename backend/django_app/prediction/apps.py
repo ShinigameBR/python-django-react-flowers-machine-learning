c8a0d7aac1fdd92bd7d98057c7ac4baa4221ecae
Expedito Hebert <shinigamecontato@gmail.com>
2025-01-10 14:08:32 -0300

    feat: :sparkles: create prediction view

--- backend/django_app/prediction/apps.py
+++ backend/django_app/prediction/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
-
+from django.conf import settings
+import pandas as pd
+from joblib import load
+import os
 
 class PredictionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prediction'
+    MLMODEL_FOLDER = os.path.join(settings.BASE_DIR, 'prediction/mlmodel/')
+    MLMODEL_FILE = os.path.join(MLMODEL_FOLDER, "IRISRandomForestClassifier.joblib")
+    mlmodel = load(MLMODEL_FILE)
--- backend/django_app/prediction/views.py
+++ backend/django_app/prediction/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from prediction.apps import PredictionConfig
+import pandas as pd
 
 # Create your views here.
+# Class based view to predict based on IRIS model
+@api_view()
+class IRIS_Model_Predict(APIView):
+    def post(self, request, format=None):
+        features = self.extract_features(request.data)
+        predicted_species = self.predict_species(features)
+        response_dict = {"Predicted Iris Species": predicted_species}
+        return Response(response_dict, status=200)
+
+    def extract_features(self, data):
+        """Extract features from the request data and convert them into a numpy array."""
+        values = list(data.values())
+        return pd.Series(values).to_numpy().reshape(1, -1)
+
+    def predict_species(self, features):
+        """Predict the Iris species based on the input features."""
+        model = PredictionConfig.mlmodel
+        y_pred = model.predict(features)
+        target_map = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+        predicted_species = pd.Series(y_pred).map(target_map).to_numpy()
+        return predicted_species[0]
